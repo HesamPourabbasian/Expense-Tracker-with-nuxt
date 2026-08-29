d93e838622fee9ee48f7ea1ee2e856538fd6331d
HesamPourabbasian <hesampourabbasian6@gmail.com>
2026-08-29 17:10:37 +0330

    fix(db): instantiate PrismaPg with pg.Pool to prevent concurrent client query deprecation warning

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,3 +1,4 @@
+import pg from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
@@ -5,17 +6,25 @@ let prisma: PrismaClient
 
 declare global {
   var __prisma: PrismaClient | undefined
+  var __pgPool: pg.Pool | undefined
 }
 
+const connectionString = process.env.DATABASE_URL!
+
 if (process.env.NODE_ENV === 'production') {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+  const pool = new pg.Pool({ connectionString })
+  const adapter = new PrismaPg(pool)
   prisma = new PrismaClient({ adapter })
 } else {
+  if (!global.__pgPool) {
+    global.__pgPool = new pg.Pool({ connectionString })
+  }
   if (!global.__prisma) {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+    const adapter = new PrismaPg(global.__pgPool)
     global.__prisma = new PrismaClient({ adapter })
   }
   prisma = global.__prisma
 }
 
 export default prisma
+
