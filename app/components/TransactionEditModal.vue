7037eaba182e9a2493b840949899400548747c2b
HesamPourabbasian <hesampourabbasian6@gmail.com>
2026-08-31 17:51:37 +0330

    perf(transactions): optimize description input in transaction edit modal

--- app/components/TransactionEditModal.vue
+++ app/components/TransactionEditModal.vue
@@ -104,6 +104,8 @@ async function handleSubmit() {
             type="text"
             class="form-control"
             placeholder="عنوان یا توضیحات تراکنش..."
+            spellcheck="false"
+            autocomplete="off"
           />
         </div>
 
